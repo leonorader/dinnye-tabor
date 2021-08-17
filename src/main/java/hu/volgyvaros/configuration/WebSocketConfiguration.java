1873d44c5649f2275c76eb111e2010f456348f24
leonorader <dernori@gmail.com>
2021-08-17 20:35:44 +0200

    api change

--- src/main/java/hu/volgyvaros/configuration/WebSocketConfiguration.java
+++ src/main/java/hu/volgyvaros/configuration/WebSocketConfiguration.java
@@ -19,6 +19,6 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(boardsHandler, "/socket/boards").setAllowedOrigins("*");
+        registry.addHandler(boardsHandler, "/boards").setAllowedOrigins("*");
     }
 }
